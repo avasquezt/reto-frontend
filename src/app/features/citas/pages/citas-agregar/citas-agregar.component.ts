6af9b8863aeaeb14714aa9a1afb953be5bc9d1ff
avasquet <alejandro.vasquez@sophossolutions.com>
2023-05-21 23:23:11 -0500

    Delete unused imports from citas-agregar component

--- src/app/features/citas/pages/citas-agregar/citas-agregar.component.ts
+++ src/app/features/citas/pages/citas-agregar/citas-agregar.component.ts
@@ -1,6 +1,4 @@
-import { Component, EventEmitter, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { Cita } from 'src/app/core/models/cita.model';
 import { HeaderService } from 'src/app/core/services/header.service';
 
@@ -14,7 +12,5 @@ export class CitasAgregarComponent {
   constructor(private headerService: HeaderService){
     this.headerService.setHeader('Citas - Nueva cita');
   }
-
-  cita: Cita = {usuario: 1, test: 2, fecha: new Date("2023-10-10T00:00"), hora: new Date(new Date().setHours(10,20))};
 
 }
