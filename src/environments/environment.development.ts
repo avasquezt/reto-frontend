2655659f5e53d0990f51dcbdd03aaedcdc183a9f
avasquet <alejandro.vasquez@sophossolutions.com>
2023-05-25 13:01:50 -0500

    Add endpoint URL's to environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    ApiUrl: {
+        base: '/api/controller',
+        affiliatesContext: '/affiliates',
+        appointmentsContext: '/appointments',
+        testsContext: '/tests',
+    }
+};
