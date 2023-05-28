3c132657562c0d0e9f9125137d33db4ecd57b2e3
avasquet <alejandro.vasquez@sophossolutions.com>
2023-05-27 22:06:17 -0500

    Add page not found component and route configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsultasComponent } from './features/consultas/consultas.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'consultas', pathMatch:'full'},
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:'citas', title: 'Citas' ,loadChildren:() => import('../app/features/citas/citas.module').then(e => e.CitasModule)},
   {path:'afiliados', title: 'Afiliados' ,loadChildren:() => import('../app/features/afiliados/afiliados.module').then(e => e.AfiliadosModule)},
   {path:'pruebas', title: 'Pruebas' ,loadChildren:() => import('../app/features/pruebas/pruebas.module').then(e => e.PruebasModule)},
+  {path:'**', component:PageNotFoundComponent, pathMatch:'full'},
 ];
 
 @NgModule({
